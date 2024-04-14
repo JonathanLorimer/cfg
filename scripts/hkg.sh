ebae1abefc37c99de9d1b35e2018c8c2fd76308a
Jonathan Lorimer <jonathan_lorimer@mac.com>
2024-04-14 13:22:05 -0400

    fix hackage script temp dir for GHA

--- scripts/hkg.sh
+++ scripts/hkg.sh
@@ -4,7 +4,13 @@ set -e
 : "$REF_NAME"
 : "$HACKAGE_USERNAME"
 : "$HACKAGE_PASSWORD"
-TMP_DIR=$(mktemp -d)
+
+if [[ -z $GITHUB_ACTIONS ]]; then
+  TMP_DIR=$(echo $RUNNER_TEMP)
+else
+  TMP_DIR=$(mktemp -d)
+fi
+
 cabal sdist -o "$TMP_DIR"
 cabal upload -u "$HACKAGE_USERNAME" -p "$HACKAGE_PASSWORD" --publish "$TMP_DIR"/cfg-"$REF_NAME".tar.gz
 cabal haddock --builddir "$TMP_DIR" --haddock-html-location='https://hackage.haskell.org/package/$pkg-$version/docs' --haddock-hyperlink-source --haddock-quickjump --haddock-for-hackage
