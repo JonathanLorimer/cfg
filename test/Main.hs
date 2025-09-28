6b655b638a5ecd4dd71ee29153d1c111dcb42294
Jonathan Lorimer <jonathan_lorimer@mac.com>
2025-09-28 19:18:00 -0400

    fix: foramt

--- test/Main.hs
+++ test/Main.hs
@@ -4,9 +4,9 @@ import Data.Maybe (fromMaybe)
 import Spec qualified
 import System.Environment (lookupEnv)
 import Test.Hspec (parallel)
+import Test.Hspec.Api.Formatters.V3 (specdoc, useFormatter)
 import Test.Hspec.Core.Runner (Config (..))
 import Test.Hspec.Runner as TR (defaultConfig, hspecWith)
-import Test.Hspec.Api.Formatters.V3 (specdoc, useFormatter)
 import Text.Read (readMaybe)
 
 main :: IO ()
@@ -16,8 +16,10 @@ main = do
     maxResources :: Int
     maxResources = fromMaybe 8 (mText >>= readMaybe)
   let
-    cfg = useFormatter ("specdoc", specdoc)
-      TR.defaultConfig
-        { configConcurrentJobs = Just maxResources
-        }
+    cfg =
+      useFormatter
+        ("specdoc", specdoc)
+        TR.defaultConfig
+          { configConcurrentJobs = Just maxResources
+          }
   hspecWith cfg (parallel Spec.spec)
